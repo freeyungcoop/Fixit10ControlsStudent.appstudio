7b61b96d5a8a32ab6fc5a029bad66f0714cac7c5
freeyungcoop <50121590+freeyungcoop@users.noreply.github.com>
2020-03-27 04:35:31 -0500

    Update
    Update

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,11 +2,20 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+  selExcerises.clear()
+  for(i=0; i <= exercises.length - 1; i++)
+    selExercises.addItem(ecercises[i])
 }
 
 selExercises.onfocusout=function(){
-
+  var message = "You decided to choose:"
+  for (i=0; i<=selExercises.text.length - 1; i++)
+    message = message + " " + selExercises.text[i]
+    NSB.MsgBox(message)
 }
 
 
+
+btnFavFood.onclick=function(){
+  ChangeForm(favFoods)
+}
--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,14 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+favFoods.onshow=function(){
+  lstGroupFoods.clear()
+  for (i = 0; i<goodFoods.length; i++){
+    lstGroupFoods.addItem(goodFoods[i])
+    }
+}
+lstGroupFoods.onclick = function(){
+  lblFood.value = `You picked  ${goodFoods[lstGroupFoods.value]}`
+}
+btnNextForm1.onclick=function(){
+  ChangeForm(describeYou)
+}
