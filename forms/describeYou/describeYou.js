06ef040ca504e5d519fca7b5b2865af74513a040
freeyungcoop <50121590+freeyungcoop@users.noreply.github.com>
2020-03-27 04:33:44 -0500

    update
    update

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,14 @@
+btnNxtFrm2.onclick=function(){
+  ChangeForm(mobileNav)
+}
 
-
-
-
-
+btnYou.onclick=function(){
+  if (rdoPersonType.value == 0)
+    lblType.value = "I would agree that you arr a ' "+ rdoPersonType.value" ' person too!"
+    if (rdoPersonType.value == 1)
+    lblType.value = "I would agree that you arr a ' "+ rdoPersonType.value" ' person too!"
+    if (rdoPersonType.value == 2)
+    lblType.value = "I would agree that you arr a ' "+ rdoPersonType.value" ' person too!"
+    if (rdoPersonType.value == 3)
+    lblType.value = "I would agree that you arr a ' "+ rdoPersonType.value" ' person too!"
+}
